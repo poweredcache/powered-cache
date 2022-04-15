9f0a03a2e4c280ea70e52b175eb894b77be9f84d
mustafauysal <me@uysalmustafa.com>
2022-04-15 19:00:24 +0300

    normalize cache path

--- includes/utils.php
+++ includes/utils.php
@@ -567,6 +567,7 @@ function site_cache_dir() {
  */
 function get_page_cache_dir() {
 	$path = get_cache_dir() . 'powered-cache/';
+	$path = wp_normalize_path( $path );
 
 	/**
 	 * Filter page cache base directory.
