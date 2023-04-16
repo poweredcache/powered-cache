8ae31d6262bf7b61f662f69c4abc0c72c580c71a
mustafauysal <me@uysalmustafa.com>
2023-04-16 17:39:32 +0300

    Use HtmlMin package for HTML minification

--- includes/classes/FileOptimizer.php
+++ includes/classes/FileOptimizer.php
@@ -7,6 +7,7 @@
 
 namespace PoweredCache;
 
+use PoweredCache\Dependencies\voku\helper\HtmlMin;
 use const PoweredCache\Constants\POST_META_DISABLE_CSS_OPTIMIZATION;
 use const PoweredCache\Constants\POST_META_DISABLE_JS_OPTIMIZATION;
 use PoweredCache\Optimizer\CSS;
@@ -203,32 +204,8 @@ class FileOptimizer {
 	 * @see https://stackoverflow.com/a/29363569
 	 */
 	public function run_html_minify( $buffer ) {
-		$replace = [
-			// remove tabs before and after HTML tags
-			'/\>[^\S ]+/s'               => '>',
-			'/[^\S ]+\</s'               => '<',
-			// shorten multiple whitespace sequences; keep new-line characters because they matter in JS!!!
-			'/([\t ])+/s'                => ' ',
-			// remove leading and trailing spaces
-			'/^([\t ])+/m'               => '',
-			'/([\t ])+$/m'               => '',
-			// remove JS line comments (simple only); do NOT remove lines containing URL (e.g. 'src="http://server.com/"')!!!
-			'~//[a-zA-Z0-9 ]+$~m'        => '',
-			// remove empty lines (sequence of line-end and white-space characters)
-			'/[\r\n]+([\t ]?[\r\n]+)+/s' => "\n",
-			// remove empty lines (between HTML tags); cannot remove just any line-end characters because in inline JS they can matter!
-			'/\>[\r\n\t ]+\</s'          => '><',
-			// remove "empty" lines containing only JS's block end character; join with next line (e.g. "}\n}\n</script>" --> "}}</script>"
-			'/}[\r\n\t ]+/s'             => '}',
-			'/}[\r\n\t ]+,[\r\n\t ]+/s'  => '},',
-			// remove new-line after JS's function or condition start; join with next line
-			'/\)[\r\n\t ]?{[\r\n\t ]+/s' => '){',
-			'/,[\r\n\t ]?{[\r\n\t ]+/s'  => ',{',
-			// remove new-line after JS's line end (only most obvious and safe cases)
-			'/\),[\r\n\t ]+/s'           => '),'
-		];
-
-		$buffer  = preg_replace( array_keys( $replace ), array_values( $replace ), $buffer );
+		$html_min = new HtmlMin();
+		$buffer   = $html_min->minify( $buffer );
 
 		return $buffer;
 	}
