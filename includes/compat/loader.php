a9c8d92315aea63620da50755133b461b015d8b6
mustafauysal <me@uysalmustafa.com>
2023-05-25 00:19:43 +0300

    Add PhastPress compat - fixes #115

--- includes/compat/loader.php
+++ includes/compat/loader.php
@@ -37,6 +37,7 @@ function load_compat_files() {
 	require_once POWERED_CACHE_COMPAT_DIR . 'plugins/divi.php';
 	require_once POWERED_CACHE_COMPAT_DIR . 'plugins/elementor.php';
 	require_once POWERED_CACHE_COMPAT_DIR . 'plugins/woocommerce.php';
+	require_once POWERED_CACHE_COMPAT_DIR . 'plugins/phastpress.php';
 
 	if ( is_multisite() && defined( 'SUNRISE' ) && SUNRISE ) {
 		require POWERED_CACHE_COMPAT_DIR . 'domain-mapping.php';
---
+++ includes/compat/plugins/phastpress.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Compat with PhastPress plugin
+ *
+ * @package PoweredCache\Compat
+ * @link    https://wordpress.org/plugins/phastpress/
+ * @since   3.0.4
+ */
+
+namespace PoweredCache\Compat\PhastPress;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+if ( ! defined( 'PHASTPRESS_VERSION' ) ) {
+	return;
+}
+
+
+add_action( 'powered_cache_flushed', __NAMESPACE__, '\\flush_phast_cache' );
+add_action( 'powered_cache_purge_all_cache', __NAMESPACE__ . '\\flush_phast_cache' );
+add_action( 'powered_cache_clean_site_cache_dir', __NAMESPACE__ . '\\flush_phast_cache' );
+
+/**
+ * Flush PhastPress cache db
+ *
+ * @return void
+ */
+function flush_phast_cache() {
+	$cache_dir = WP_CONTENT_DIR . '/cache/';
+
+	$db_files = glob( $cache_dir . 'phast.*/*sqlite3*' );
+	foreach ( $db_files as $file ) {
+		@unlink( $file ); // phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged
+	}
+}
