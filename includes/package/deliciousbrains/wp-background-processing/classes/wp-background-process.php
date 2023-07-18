bc125ba3fc5c7cb3662a410dc3ea9105472849bb
mustafauysal <me@uysalmustafa.com>
2023-07-19 01:02:25 +0300

    Update background processing package

--- includes/package/deliciousbrains/wp-background-processing/classes/wp-background-process.php
+++ includes/package/deliciousbrains/wp-background-processing/classes/wp-background-process.php
@@ -527,8 +527,8 @@ abstract class Powered_Cache_WP_Background_Process extends Powered_Cache_WP_Asyn
 				// Let the server breathe a little.
 				sleep( $throttle_seconds );
 
-				if ( $this->time_exceeded() || $this->memory_exceeded() ) {
-					// Batch limits reached.
+				// Batch limits reached, or pause or cancel request.
+				if ( $this->time_exceeded() || $this->memory_exceeded() || $this->is_paused() || $this->is_cancelled() ) {
 					break;
 				}
 			}
@@ -537,7 +537,7 @@ abstract class Powered_Cache_WP_Background_Process extends Powered_Cache_WP_Asyn
 			if ( empty( $batch->data ) ) {
 				$this->delete( $batch->key );
 			}
-		} while ( ! $this->time_exceeded() && ! $this->memory_exceeded() && ! $this->is_queue_empty() );
+		} while ( ! $this->time_exceeded() && ! $this->memory_exceeded() && ! $this->is_queue_empty() && ! $this->is_paused() && ! $this->is_cancelled() );
 
 		$this->unlock_process();
 
