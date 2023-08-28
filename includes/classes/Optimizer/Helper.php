d465768574c543a2d26c5a6fba7e4f1aa578daca
mustafauysal <me@uysalmustafa.com>
2023-08-28 16:23:49 +0300

    Add exclusion filters for JS & CSS

--- includes/classes/Optimizer/Helper.php
+++ includes/classes/Optimizer/Helper.php
@@ -112,8 +112,7 @@ class Helper {
 	 * @return bool
 	 */
 	public static function is_excluded_js( $src ) {
-		$settings       = \PoweredCache\Utils\get_settings();
-		$excluded_files = preg_split( '#(\r\n|\n|\r)#', $settings['excluded_js_files'], - 1, PREG_SPLIT_NO_EMPTY );
+		$excluded_files = self::get_excluded_js();
 		$excluded_files = implode( '|', $excluded_files );
 
 		if ( ! empty( $excluded_files ) && preg_match( '#(' . $excluded_files . ')#', $src ) ) {
@@ -131,8 +130,7 @@ class Helper {
 	 * @return bool
 	 */
 	public static function is_excluded_css( $src ) {
-		$settings       = \PoweredCache\Utils\get_settings();
-		$excluded_files = preg_split( '#(\r\n|\n|\r)#', $settings['excluded_css_files'], - 1, PREG_SPLIT_NO_EMPTY );
+		$excluded_files = self::get_excluded_css();
 		$excluded_files = implode( '|', $excluded_files );
 
 		if ( ! empty( $excluded_files ) && preg_match( '#(' . $excluded_files . ')#', $src ) ) {
@@ -230,6 +228,52 @@ class Helper {
 		 * @since  3.2
 		 */
 		return (array) apply_filters( 'powered_cache_delay_exclusions', $excluded_files );
+	}
+
+	/**
+	 * Get JS exclusion list
+	 *
+	 * @return array
+	 * @since 3.2
+	 */
+	public static function get_excluded_js() {
+		$settings       = \PoweredCache\Utils\get_settings();
+		$excluded_files = preg_split( '#(\r\n|\n|\r)#', $settings['excluded_js_files'], - 1, PREG_SPLIT_NO_EMPTY );
+
+		/**
+		 * Filter the excluded JS files
+		 *
+		 * @hook   powered_cache_fo_excluded_js_files
+		 *
+		 * @param  {array} $settings Excluded files
+		 *
+		 * @return {array} New value
+		 * @since  3.2
+		 */
+		return (array) apply_filters( 'powered_cache_fo_excluded_js_files', $excluded_files );
+	}
+
+	/**
+	 * Get CSS exclusion list
+	 *
+	 * @return array
+	 * @since 3.2
+	 */
+	public static function get_excluded_css() {
+		$settings       = \PoweredCache\Utils\get_settings();
+		$excluded_files = preg_split( '#(\r\n|\n|\r)#', $settings['excluded_css_files'], - 1, PREG_SPLIT_NO_EMPTY );
+
+		/**
+		 * Filter the excluded css files
+		 *
+		 * @hook   powered_cache_fo_excluded_css_files
+		 *
+		 * @param  {array} $settings Excluded files
+		 *
+		 * @return {array} New value
+		 * @since  3.2
+		 */
+		return (array) apply_filters( 'powered_cache_fo_excluded_css_files', $excluded_files );
 	}
 
 
