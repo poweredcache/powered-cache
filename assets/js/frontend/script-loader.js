b2a6b25820e762c061b87d2bd4355636e21db346
mustafauysal <me@uysalmustafa.com>
2023-08-29 00:19:59 +0300

    lazy script loader improvements

--- assets/js/frontend/script-loader.js
+++ assets/js/frontend/script-loader.js
@@ -1,14 +1,21 @@
 /**
  * Delay JS Script Loader
  */
-'use strict';
 
 class PCScriptLoader {
 	constructor(delay) {
 		this.loadDelay = delay;
 		this.loadTimer = null;
 		this.scriptsLoaded = false;
-		this.triggerEvents = ["mouseover", "click", "keydown", "wheel", "touchmove", "touchstart", "touchend"];
+		this.triggerEvents = [
+			'mouseover',
+			'click',
+			'keydown',
+			'wheel',
+			'touchmove',
+			'touchstart',
+			'touchend',
+		];
 		this.userEventHandler = this.triggerLoader.bind(this);
 		this.init();
 	}
@@ -16,7 +23,7 @@ class PCScriptLoader {
 	init() {
 		const self = this;
 		for (const event of this.triggerEvents) {
-			window.addEventListener(event, this.userEventHandler, {passive: true});
+			window.addEventListener(event, this.userEventHandler, { passive: true });
 		}
 		if (this.loadDelay > 0) {
 			this.loadTimer = setTimeout(() => {
@@ -30,23 +37,27 @@ class PCScriptLoader {
 		this.loadScripts();
 		clearTimeout(this.loadTimer);
 		for (const event of this.triggerEvents) {
-			window.removeEventListener(event, this.userEventHandler, {passive: true});
+			window.removeEventListener(event, this.userEventHandler, { passive: true });
 		}
 	}
 
 	loadScripts() {
 		this.scriptsLoaded = true;
 		this.loadScriptsWithType("data-type='lazy'");
-		this.loadScriptsWithType("defer", true);
-		console.log("Script(s) loaded with delay or interaction");
+		this.loadScriptsWithType('defer', true);
+		console.log('Script(s) loaded with delay or interaction');
 	}
 
 	loadScriptsWithType(selector, defer = false) {
 		const scripts = document.querySelectorAll(`script[${selector}]`);
-		const loadScript = script => {
-			script.setAttribute("src", script.getAttribute("data-src"));
-			script.removeAttribute("data-src");
-			script.setAttribute("data-lazy-loaded", "true");
+		const loadScript = (script) => {
+			const src = script.getAttribute('data-src');
+			if (src === null) {
+				return;
+			}
+			script.setAttribute('src', src);
+			script.removeAttribute('data-src');
+			script.setAttribute('data-lazy-loaded', 'true');
 		};
 
 		if (defer) {
@@ -59,3 +70,5 @@ class PCScriptLoader {
 	}
 }
 
+const timeout = window.pcScriptLoaderTimeout || 0;
+window.PCScriptLoader = new PCScriptLoader(timeout);
